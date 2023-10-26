5834f12682677d5336d002763ad27f48df9d42bf
Rémy Frèrebeau <77391972+rfrerebe-stx@users.noreply.github.com>
2023-10-26 15:29:54 +0200

    use buf generate to get docs.json

--- docs.ps1
+++ docs.ps1
@@ -1,6 +1,3 @@
 #/usr/bin/pwsh
 
-$path = Join-Path -Path $pwd -ChildPath "src"
-$items = Get-ChildItem -Recurse -Include *.proto | % { $_.FullName.Replace($path,"protos").Replace('\','/') }
-
-docker run --rm -v ${pwd}\doc:/out -v ${pwd}\src:/protos  pseudomuto/protoc-gen-doc --doc_opt=json,docs.json --proto_path=/protos $items
+docker run --rm -v ${pwd}:/protos -v ${pwd}\gen:/gen --workdir /protos bufbuild/buf generate src
